0b43bd80d1120bdccc2b014fb0a291341cb69d9c
KDT <developer.thiru@gmail.com>
2025-04-01 16:49:15 -0400

    recursive page crawling

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,4 +21,81 @@ func main() {
 	}
 
 	fmt.Printf("starting crawl of: %s\n", os.Args[1])
+
+	pages := make(map[string]int) // map for storing link counts
+	crawlPage(os.Args[1], os.Args[1], pages)
+
+	fmt.Println(pages)
+}
+
+func getHTML(raw_url string) (string, error) {
+	resp, err := http.Get(raw_url)
+	if err != nil {
+		return "", err
+	}
+
+	// Check for HTTP status error
+	if resp.StatusCode >= 400 {
+		return "", fmt.Errorf("error response code: %d", resp.StatusCode)
+	}
+
+	// Check for proper content type
+	if !strings.HasPrefix(resp.Header.Get("Content-Type"), "text/html") {
+		return "", fmt.Errorf("error invalid content type: %s", resp.Header.Get("Content-Type"))
+	}
+	defer resp.Body.Close()
+
+	htmlData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(htmlData), nil
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	// Check if rawCurrentURL has the same domain as rawBaseURL check
+	baseWithoutPrefix := strings.TrimPrefix(rawBaseURL, "https://")
+	baseDomain := strings.Split(baseWithoutPrefix, "/")[0]
+
+	currentWithoutPrefix := strings.TrimPrefix(rawCurrentURL, "https://")
+	currentDomain := strings.Split(currentWithoutPrefix, "/")[0]
+
+	fmt.Printf("Base domain: %s\r\n", baseDomain)
+	fmt.Printf("Current domain: %s\r\n", currentDomain)
+
+	if baseDomain != currentDomain {
+		return
+	}
+
+	normalizedCurrent, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		fmt.Println("error normalizeURL(rawCurrentURL) failed:", err)
+		return
+	}
+
+	// Check if already crawled
+	if _, ok := pages[normalizedCurrent]; ok {
+		pages[normalizedCurrent] += 1
+		return
+	} else {
+		pages[normalizedCurrent] = 1
+	}
+
+	htmlData, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Println("error getHTML(rawCurrentURL) failed:", err)
+		return
+	}
+	fmt.Printf("HTML data for %s:\r\n%s\r\n", rawCurrentURL, htmlData)
+
+	urls, err := getURLsFromHTML(htmlData, rawBaseURL)
+	if err != nil {
+		fmt.Println("error getURLsFromHTML() failed:", err)
+		return
+	}
+
+	for _, url := range urls {
+		crawlPage(rawBaseURL, url, pages)
+	}
 }
